3e08b6561cc2b778c4782e9beea1b7726500b277
Trần Thế Vũ <anhvaut@gmail.com>
2018-07-07 14:07:54 +0700

    update test file

---
+++ test-read-serial.py
@@ -0,0 +1,10 @@
+import serial
+import os
+f = os.popen('ls /dev/ttyACM*')
+now = f.read()
+now=now[:-1]
+print now
+#os.system('sudo minicom')
+ser = serial.Serial(now,9600)
+While 1:
+ ser.readline()
